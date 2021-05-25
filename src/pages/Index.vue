e375a5264b65a81889271f1142dfbeff020d1fb4
Nurjadein <nurjadeinabdulmorid@gmail.com>
2021-05-25 10:42:45 +0800

    cleaning up the page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,7 +10,6 @@
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/ClassComponent.vue';
 import { Vue, Component } from 'vue-property-decorator';
 
@@ -18,30 +17,6 @@ import { Vue, Component } from 'vue-property-decorator';
   components: { ExampleComponent }
 })
 export default class PageIndex extends Vue {
-  todos: Todo[] = [
-    {
-      id: 1,
-      content: 'ct1'
-    },
-    {
-      id: 2,
-      content: 'ct2'
-    },
-    {
-      id: 3,
-      content: 'ct3'
-    },
-    {
-      id: 4,
-      content: 'ct4'
-    },
-    {
-      id: 5,
-      content: 'ct5'
-    }
-  ];
-  meta: Meta = {
-    totalCount: 1200
-  };
+ 
 };
 </script>
